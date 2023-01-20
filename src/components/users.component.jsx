75e44f2d3a53002edd8674f947e7b1809d618b55
matias villarroel <matiasvillarroel@Air-de-Matias.local>
2023-01-20 15:50:21 -0300

    hola 5

--- src/components/users.component.jsx
+++ src/components/users.component.jsx
@@ -2,9 +2,9 @@ const Users = () => {
   return (
     <div className="user-container">
       <h1>Hola desde el UsersComponent</h1>
-      <p>hola mundo cristian</p>
+      <p>hola mundo cristian xd</p>
       <p>Hello word</p>
-      <h3>Hola Mati</h3>
+      <h3>Hola Mati ;</h3>
     </div>
   );
 };
