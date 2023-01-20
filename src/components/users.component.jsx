9d14129a4cd065295a3544531227e7c36933fc6a
Cristian Espinoza <cristian.espinoza@arkavia.com>
2023-01-20 15:50:52 -0300

    add className

--- src/components/users.component.jsx
+++ src/components/users.component.jsx
@@ -2,7 +2,7 @@ const Users = () => {
   return (
     <div className="user-container">
       <h1>Hola desde el UsersComponent</h1>
-      <p>hola mundo cristian</p>
+      <p className="user-p">hola mundo cristian</p>
       <p>Hello word</p>
       <h3>Hola Mati</h3>
     </div>
